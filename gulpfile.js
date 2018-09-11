980c22af18715beb0dc79dd2f53687a052468681
Xande0812 <xande0812@gmail.com>
2018-09-11 17:00:27 +0900

    add sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var plumber = require('gulp-plumber');
 var notify = require("gulp-notify");
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 /**
  * 開発用のディレクトリを指定します。
@@ -63,7 +64,9 @@ gulp.task('html', function() {
  */
 gulp.task('sass', function () {
  return gulp.src(src.sass, {base: src.root})
+   .pipe(sourcemaps.init())
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(sourcemaps.write())
    .pipe(gulp.dest(dest.root))
    .pipe(browserSync.reload({stream: true}));
 });
